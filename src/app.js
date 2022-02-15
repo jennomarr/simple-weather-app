65b102b6994c8927ddb11290705dff41c5cb3432
jennomarr <jennomarrr@gmail.com>
2022-02-15 13:43:56 -0500

    Moved weather forecast section from HTML to JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,32 @@ http: function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForcast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thurs", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2">
+         <div class="weather-forcast-date">${day}</div>
+            <img
+              src="https://openweathermap.org/img/wn/01d@2x.png"
+              alt=""
+              width="40px"/>
+          <div class="weather-forcast-temperature">
+              <span class="weather-forcast-max">18°</span>
+              <span class="weather-forcast-min">12°</span>
+              </div>
+              </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForcast();
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
