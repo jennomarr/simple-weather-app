27bbb76e722a056ae8768f06ceafba33b6af8365
jennomarr <jennomarrr@gmail.com>
2022-02-17 09:50:53 -0500

    Add weather forecast API

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ http: function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForcast() {
+function displayForcast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thurs", "Fri", "Sat", "Sun"];
   let forecastHTML = `<div class="row">`;
@@ -46,7 +47,13 @@ function displayForcast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForcast();
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "fe730648f8a35b4d2322ddefb6bc509c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForcast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -69,6 +76,7 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
