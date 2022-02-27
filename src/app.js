8d579eae2763a5cf56a9c6a19852cc7225088595
jennomarr <jennomarrr@gmail.com>
2022-02-27 14:17:16 -0500

    Remove temperature conversion

--- src/app.js
+++ src/app.js
@@ -110,31 +110,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-function dispayFahrenheittemperature(event) {
-  event.preventDefault();
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function dispayCelsiustemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  fahrenheitLink.classList.remove("active");
-  celsiusLink.classList.add("active");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-let celsiusTemperature = null;
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", dispayFahrenheittemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", dispayCelsiustemperature);
 
 search("Toronto");
